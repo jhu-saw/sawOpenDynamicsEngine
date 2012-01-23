ec8bd3bb8166ece04753a9dfee757d321145ca3c
Jonathan Bohren <jbo@jhu.edu>
2012-01-23 22:25:06 +0000

    Index: pkg/packages.yaml   Updated packag specifications.
    Index: pkg/gen_pkgs.py
      Lots of updates to packaging script.
    
    Index: saw/components/sawOpenDynamicsEngine/code/cmake/Modules/FindOpenDynamicsEngine.cmake
      Added cmake min version
      Added code to get the right ode libs on ubuntu/devian
    
    Index: saw/components/sawOpenDynamicsEngine/code/CMakeLists.txt
      Added hints to find sawOpenSceneGraph
      Added include line to get cisst cmake macros
     
    Index: saw/components/sawOpenDynamicsEngine/examples/CMakeLists.txt
      Fixed hints for sawOpenDynamicsEngine cisst_saw_BINARY_DIR -> saw_BINARY_DIR so that it now compiles outside of the cisst cmake project
     
    Index: saw/components/sawKeyboard/code/CMakeLists.txt
      Added install targets for sawKeyboard
    
    Index: saw/components/sawOpenSceneGraph/code/CMakeLists.txt
      Fixed use of feature only found cmake-2.8.4 when finding libraries
      Added install target for sawOpenSceneGraph
    
    Index: saw/components/sawOpenSceneGraph/examples/CMakeLists.txt
      Fixed hints for sawOpenSceneGraph cisst_saw_BINARY_DIR -> saw_BINARY_DIR so that it now compiles outside of the cisst cmake project
     
    Index: saw/CMakeLists.txt
      Added some CPack stuff to the saw project for packaging
    
    
    
    git-svn-id: https://svn.lcsr.jhu.edu/cisst/trunk/saw/components/sawOpenDynamicsEngine@3370 0959eb1a-6956-4f60-947b-6092a4e22c97

--- code/cmake/Modules/FindOpenDynamicsEngine.cmake
+++ code/cmake/Modules/FindOpenDynamicsEngine.cmake
@@ -1,3 +1,4 @@
+cmake_minimum_required(VERSION 2.6.0)
 
 if (WIN32)
   
@@ -7,19 +8,22 @@ if (WIN32)
   find_library( ODE_LIBRARY NAMES ode 
     PATHS ENV ODE_ROOT_PATH PATH_SUFFIXES lib )
 
-else (WIN32)
+elseif (APPLE)
+  # TODO?
+elseif (UNIX)
 
-  if (APPLE)
+  find_path( ODE_INCLUDE_DIR ode/ode.h )
 
+  # On RedHat, the ode double-precision library is called "ode-double" and
+  # the single-precision is called "ode" but on Debian/Ubuntu they are
+  # called "ode" and "ode-sp", repsectively
+  find_library( ODE_LIBRARY ode-double ode )
 
-  else (APPLE)
+  if (ODE_LIBRARY STREQUAL "ODE_LIBRARY-NOTFOUND")
+    find_library( ODE_LIBRARY ode )
+  endif ()
 
-    find_path( ODE_INCLUDE_DIR ode/ode.h )
-    find_library( ODE_LIBRARY ode-double ode )
-
-  endif (APPLE)
-
-endif (WIN32)
+endif ()
 
 set( ODE_FOUND FALSE )
 
